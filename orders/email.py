c268ffa6ad8c8aa508379c38c29f2a9419b0311a
Lubomir92 <szlovakl333@gmail.com>
2026-07-30 22:44:17 +0200

    Use HTML email templates

--- orders/email.py
+++ orders/email.py
@@ -1,15 +1,18 @@
 import os
 import resend
+from django.template.loader import render_to_string
 
 
 def send_order_email(order, subject, template):
     try:
+        html = render_to_string(template, {"order": order})
+        
         resend.api_key = os.getenv("RESEND_API_KEY")
         resend.Emails.send({
             "from": "Velvet Creature <onboarding@resend.dev>",
             "to": [order.email],
             "subject": subject,
-            "text": f"Order: {order.order_number}\nStatus: {order.status}\nTotal: €{order.total_price}",
+            "html": html,
         })
     except Exception as e:
         print(f"Email error: {e}")
