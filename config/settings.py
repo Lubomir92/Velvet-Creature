77ad870fa9f6882c459959ec620db2605df78738
Lubomir92 <szlovakl333@gmail.com>
2026-07-30 22:12:20 +0200

    Fix static files with Cloudinary

--- config/settings.py
+++ config/settings.py
@@ -177,6 +177,7 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
