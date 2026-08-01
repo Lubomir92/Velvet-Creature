ae0687529c1dc1a3a17e2c0410d550e7719d25c0
Lubomir92 <szlovakl333@gmail.com>
2026-08-01 12:37:47 +0200

    Fix email.py - wrong content

--- orders/email.py
+++ orders/email.py
@@ -1,106 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from django.conf.urls.i18n import i18n_patterns
-
-from django.conf import settings
-from django.conf.urls.static import static
+import os
+import resend
+from django.template.loader import render_to_string
 
 
-
-# ==========================================
-# NON LANGUAGE URLS
-# ==========================================
-
-urlpatterns = [
-
-    # Language switch
-    path(
-        "i18n/",
-        include("django.conf.urls.i18n")
-    ),
-
-
-    # Django admin
-    path(
-        "admin/",
-        admin.site.urls
-    ),
-
-]
-
-
-
-# ==========================================
-# MAIN WEBSITE URLS
-# ==========================================
-
-urlpatterns += i18n_patterns(
-    prefix_default_language=False,
-
-    # HOME
-    path(
-        "",
-        include("core.urls")
-    ),
-
-
-    # SHOP
-    path(
-        "shop/",
-        include("products.urls")
-    ),
-
-
-    # CART
-    path(
-        "cart/",
-        include("cart.urls")
-    ),
-
-
-    # ORDERS
-    path(
-        "orders/",
-        include("orders.urls")
-    ),
-
-
-    # ACCOUNTS
-    path(
-        "accounts/",
-        include("accounts.urls")
-    ),
-
-
-    # CUSTOM ORDERS
-    path(
-        "custom/",
-        include("custom_orders.urls")
-    ),
-
-
-    # WISHLIST
-    path(
-        "wishlist/",
-        include("wishlist.urls")
-    ),
-
-
-)
-
-
-
-# ==========================================
-# STATIC & MEDIA FILES
-# ==========================================
-
-urlpatterns += static(
-    settings.STATIC_URL,
-    document_root=settings.STATIC_ROOT
-)
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+def send_order_email(order, subject, template):
+    try:
+        html = render_to_string(template, {"order": order})
+        
+        resend.api_key = os.getenv("RESEND_API_KEY")
+        resend.Emails.send({
+            "from": "Velvet Creature <onboarding@resend.dev>",
+            "to": [order.email],
+            "subject": subject,
+            "html": html,
+        })
+    except Exception as e:
+        print(f"Email error: {e}")
