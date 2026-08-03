232be9ecceabf343dcac884f0c118ef3cfcff847
Lubomir92 <szlovakl333@gmail.com>
2026-08-04 01:01:01 +0200

    Force redirect to French

--- core/views.py
+++ core/views.py
@@ -1,21 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from products.models import Product
 
 
 def home(request):
+    # Presmerovanie na francúzsku verziu
+    return redirect('/fr/')
 
-    featured_products = Product.objects.filter(
-        featured=True
-    ).order_by("-created")[:3]
-
-
-    return render(
-        request,
-        "products/home.html",
-        {
-            "featured_products": featured_products,
-        }
-    )
 
 def about(request):
     return render(
